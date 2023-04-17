64e8f3a36257e8bd90ea59215579da6fcee883e0
just_v <just_v@inbox.ru>
2023-04-18 01:37:44 +0500

    fix9

--- index.js
+++ index.js
@@ -27,76 +27,55 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// interface IGlobHeaderRes {
-//   header: (a: string, b: string) => void;
-// }
+app.all('*', (req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://telegram-webapp-client.vercel.app'
+  );
+  res.header('Vary', 'Origin');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
-// app.all('*', (req: any, res: any, next: any) => {
-//   res.header('Access-Control-Allow-Origin', `${process.env.ACAO}`);
-//   res.header('Vary', 'Origin');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
-
-async function getPhotosListByTagName(userId: string) {
+async function getPhotosListByTagName(userId) {
   return await knex('saved_image')
     .select()
     .where({ sender_tg_id: userId })
-    .then((res: { tag: string }) => {
+    .then((res) => {
       return res;
     });
 }
 
-async function getBlobFromImage(url: string) {
+async function getBlobFromImage(url) {
   const { Blob } = require('buffer');
   const result = await requester(url)
-    .then((res: any) => {
+    .then((res) => {
       return res.buffer();
     })
-    .then((res: any) => {
+    .then((res) => {
       return new Blob([res], { type: 'image/png' });
     })
-    .catch((err: any) => {
+    .catch((err) => {
       return err;
     });
 
   return result;
 }
 
-interface ISearchReq {
-  body: {
-    tag: string;
-    userId: string;
-  };
-}
-
-interface ISearchRes {
-  status: (e: number) => {
-    send: (
-      e?:
-        | {
-            botId?: string;
-            photosList?: string[];
-          }
-        | string
-    ) => void;
-  };
-}
-
 app.post(
   '/search',
   bodyParser.urlencoded({ extended: false }),
-  async (req: ISearchReq, res: ISearchRes) => {
+  async (req, res) => {
     try {
       const { userId, tag } = req.body;
       const photosList = await getPhotosListByTagName(userId);
-      const filtredList = photosList.filter((item: { tag: string }) =>
+      const filtredList = photosList.filter((item) =>
         String(item.tag).toLowerCase().includes(tag.toLowerCase())
       );
       console.log(userId, tag);
       await Promise.all(
-        await filtredList.map(async (item: { image_id: string }) => {
+        await filtredList.map(async (item) => {
           return (await bot.getFile(item.image_id)).file_path;
         })
       )
@@ -104,54 +83,35 @@ app.post(
           res.status(200).send({ botId: botToken, photosList: response });
         })
         .catch((err) => res.status(404).send(String(err)));
-    } catch (err: any) {
+    } catch (err) {
       res.status(400).send(err.message);
     }
   }
 );
 
-interface IPhotoReq {
-  body: {
-    url: string;
-  };
-}
-
-interface IPhotoRes {
-  status: (e: number) => {
-    type: (e: IBlob) => {};
-    send: (e: string) => void;
-  };
-  send: (e: Buffer) => void;
-  sendStatus: (e: number) => void;
-}
-
-interface IBlob extends Blob {
-  type: 'image/png';
-}
-
 app.post(
   '/photo',
   bodyParser.urlencoded({ extended: false }),
-  async (req: IPhotoReq, res: IPhotoRes) => {
+  async (req, res) => {
     try {
       const url = req.body.url;
       const blob = await getBlobFromImage(url);
 
       if (blob.constructor.name === 'Blob') {
         res.status(201).type(blob.type);
-        blob.arrayBuffer().then((buf: any) => {
+        blob.arrayBuffer().then((buf) => {
           res.send(Buffer.from(buf));
         });
       } else {
         res.sendStatus(400);
       }
-    } catch (err: any) {
+    } catch (err) {
       res.status(400).send(err.message);
     }
   }
 );
 
-app.get('/test', async (req: any, res: any) => {
+app.get('/test', async (req, res) => {
   try {
     res.header('Access-Control-Allow-Origin', '*').status(200).send('test: ok');
   } catch (err) {
@@ -159,13 +119,7 @@ app.get('/test', async (req: any, res: any) => {
   }
 });
 
-interface IUseGlobErrorRes {
-  status: (e: number) => {
-    send: (e: string) => void;
-  };
-}
-
-app.use((err: any, req: any, res: IUseGlobErrorRes, next: any) => {
+app.use((err, req, res, next) => {
   res.status(500).send(String(err.message));
 
   next();
